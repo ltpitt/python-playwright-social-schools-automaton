3e2f6cc23d4e1f2f9f90e25ea8ba016a0aba7bab
Davide Nastri <d.nastri@gmail.com>
2025-03-25 23:22:09 +0100

    Delete config.example.py

--- config.example.py
+++
@@ -1,4 +0,0 @@
-SCRAPED_WEBSITE_USER = "your_email@example.com"
-SCRAPED_WEBSITE_PASSWORD = "your_password"
-PUSHBULLET_API_KEY = "your_pushbullet_api_key"
-TRANSLATION_LANGUAGE = "en"  # Use en, fr, it, etc. 
